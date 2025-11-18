d692292d4b77c92c03abc35798e2f155a4bbb5e2
Ahmed Sualih <sualihahmed26@gmail.com>
2025-11-18 11:43:19 +0300

    adding planets project

---
+++ Planets-Project/index.js
@@ -0,0 +1,33 @@
+const {parse} = require("csv-parse");
+const fs = require('fs');
+
+const habitablePlanets = [];
+// More lenient criteria
+function isHabitablePlanet(planet) {
+    return planet['koi_disposition'] === "CONFIRMED" 
+        && planet['koi_insol'] > 0.32  
+        && planet['koi_insol'] < 1.5  
+        && planet['koi_prad'] < 2.0;
+}
+fs.createReadStream('kepler_data.csv')
+  .pipe(parse({
+    comment: '#',      // Treat lines starting with # as comments
+    columns: true,     // Use first row as column names
+    skip_empty_lines: true // Skip empty lines
+  }))
+  .on('data', (data) => {
+ if(isHabitablePlanet(data)){
+    habitablePlanets.push(data);
+ }
+ 
+  })
+  .on('error', (err) => {
+    console.log(err);
+  })
+  .on('end', () => {
+console.log(habitablePlanets.map((planet)=>{
+  return planet["kepler_name"];
+}))
+console.log(`${habitablePlanets.length} habitable planets found`)
+    console.log("Done");
+  });
